05b1467f71d70f35e48ba05e7ecd2f5e31f5ae4f
AndreasHjorth11 <77662628+AndreasHjorth11@users.noreply.github.com>
2021-11-01 15:02:04 +0100

    Add Swipey Update

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const Joke = document.querySelector(".jokeItem");
-let touchCordinatesStart;
-let touchCordinatesMove;
+let touchCoordinatesStart;
+let touchCoordinatesMove;
+let touchCoordinatesEnd;
+let deleteButton = window.screen.width * 0.3;
+
 Joke.addEventListener("touchstart", (e) => {
-    console.log(e.touches[0].clientX);
-    touchCordinatesStart = e.touches[0].clientX;
+    //console.log(e.touches[0].clientX);
+    touchCoordinatesStart = e.touches[0].clientX;
 });
 
 Joke.addEventListener("touchmove", (e) => {
-    console.log(e.touches[0].clientX);
-    touchCordinatesMove = Math.floor(e.touches[0].clientX);
-    if (touchCordinatesMove < touchCordinatesStart) {
+    //console.log(e.touches[0].clientX);
+    touchCoordinatesMove = Math.floor(e.touches[0].clientX);
+    if (touchCoordinatesMove < touchCoordinatesStart && 
+        touchCoordinatesMove > touchCoordinatesStart - deleteButton) {
 
-        Joke.style.transform = `translateX(${touchCordinatesMove - touchCordinatesStart}px)`;
+        Joke.style.transform = `translateX(${touchCoordinatesMove - touchCoordinatesStart}px)`;
     }
     //Joke.style.transform = 'translateX(-100px)'
 
+});
+
+Joke.addEventListener('touchend', (e) => {
+    touchCoordinatesEnd = Math.floor(e.changedTouches[0].clientX);
+    if (touchCoordinatesEnd < touchCoordinatesStart - deleteButton / 2 ) {
+        //console.log("test");
+        Joke.style.transform = `translateX(-${deleteButton}px)`
+    }
+    else {
+        Joke.style.transform = `translateX(0px)`
+    }
 });
